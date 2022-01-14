a83b70b2bb52bbf4d4e58644650fa4bbf79661ea
Joshua Heng <hengj@nextcapital.com>
2022-01-13 19:58:16 -0600

    Handle case when tab is on an chrome page like chrome://...

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
   if (info.status == "complete") {
-    chrome.tabs.executeScript(tabId, {file: 'content.js'});
+    chrome.tabs.executeScript(tabId, {file: 'content.js'}, _=> {
+      let e = chrome.runtime.lastError;
+      if(e !== undefined){
+        console.log(tabId, _, e);
+      }
+    });
   }
 });

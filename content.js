cf23ce8cb31172144acb615d113d0e78875584ca
Joshua Heng <hengj@nextcapital.com>
2022-01-27 00:56:10 -0600

    Introduce MutationObserver to update JIRA title when the DOM changes - Covers the case when a PR title is updated manually - Covers the case when a PR title does not have a JIRA ID

--- content.js
+++ content.js
@@ -1,27 +1,62 @@
 (() => {
-  const $title = document.querySelector('.js-issue-title');
-  if (!$title) {
-    return;
+  const updateIssueTitle = function() {
+    const $title = document.querySelector('.js-issue-title');
+    const regex = /\[\w+-\d+\]/g;
+    const hasJiraId = $title.innerHTML.match(regex);
+
+    // Skips execution if
+    // 1. it is not on a github PR page
+    // 2. the PR title does not contain a JIRA ID (also prevents an infinite loop)
+    // 3. the PR title already contains a href
+    if (!$title || !hasJiraId || $title.innerHTML.includes('href')) {
+      return;
+    }
+
+    chrome.storage.local.get(['jiraUrl', 'inlineLinks'], (options) => {
+      const jiraUrl = !!options.jiraUrl ?
+        options.jiraUrl :
+        'https://jira.nextcapital.com';
+
+      let title = $title.innerHTML.replace(/(<a[^>]+>|⬆︎|<\/a>)/g, '');
+
+      title = title.replace(/[a-zA-Z0-9-]+(?=[\],\s\d#]*\])/g, (tag) => {
+        const url = `${jiraUrl}/browse/${tag}`;
+        const attrs = `href="${url}" target="_blank"`;
+
+        const replacement = options.inlineLinks === false ?
+          `${tag}<a ${attrs}>⬆︎</a>` :
+          `<a ${attrs}>${tag}</a>`;
+
+        return replacement;
+      });
+
+      $title.innerHTML = title;
+    });
   }
 
-  chrome.storage.local.get(['jiraUrl', 'inlineLinks'], (options) => {
-    const jiraUrl = !!options.jiraUrl ?
-      options.jiraUrl :
-      'https://jira.nextcapital.com';
+  updateIssueTitle();
 
-    let title = $title.innerHTML.replace(/(<a[^>]+>|⬆︎|<\/a>)/g, '');
+  // Select the node that will be observed for mutations
+  const targetNode = document.querySelector('.js-issue-title');
 
-    title = title.replace(/[a-zA-Z0-9-]+(?=[\],\s\d#]*\])/g, (tag) => {
-      const url = `${jiraUrl}/browse/${tag}`;
-      const attrs = `href="${url}" target="_blank"`;
+  if (targetNode) {
+    // Options for the observer (which mutations to observe)
+    const config = { childList: true, subtree: true };
 
-      const replacement = options.inlineLinks === false ?
-        `${tag}<a ${attrs}>⬆︎</a>` :
-        `<a ${attrs}>${tag}</a>`;
+    // Callback function to execute when mutations are observed
+    const callback = function(mutationsList, observer) {
+      // Use traditional 'for loops' for IE 11
+      for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          updateIssueTitle();
+        }
+      }
+    };
 
-      return replacement;
-    });
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
 
-    $title.innerHTML = title;
-  });
+    // Start observing the target node for configured mutations
+    observer.observe(document, config);
+  }
 })();
